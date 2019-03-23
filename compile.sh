bd7edf16fe689608417d99031cd1fd74d7187ca0
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-03-23 16:30:44 +0000

    Add script to run jjtree, javacc, javac and launch syntax analyzer

---
+++ compile.sh
@@ -0,0 +1,15 @@
+JJT_FILE="teste.jjt"
+JJ_FILE="teste.jj"
+SYNTAX_ANALYZER_FILE="Fac"
+
+# Run JJTree
+jjtree $JJT_FILE || exit 1
+
+# Generate java code
+javacc $JJ_FILE || exit 2
+
+# Compile generated java code
+javac *.java || exit 3
+
+# Run syntax analyzer
+java $SYNTAX_ANALYZER_FILE || exit 4
